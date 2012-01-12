1fb72ee2a993e06d00e1f50116c7519dad1235d9
fitsnugly <rickfitsnugly@gmail.com>
2012-01-12 01:10:39 -0500

    vendor: add camera effects prebuilts
    Change-Id: Ib9ea7c84f43eb5e0451cfecd41d8885e901ea8d4
    
    Conflicts:
    	config/common.mk

--- configs/system_files.mk
+++ configs/system_files.mk
@@ -2,4 +2,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/disco/overlay/
 
 PRODUCT_COPY_FILES += \
 vendor/disco/prebuilt/system/etc/apns-conf.xml:system/etc/apns-conf.xml \
-vendor/disco/prebuilt/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+vendor/disco/prebuilt/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+vendor/disco/prebuilt/system/media/LMprec_508.emd:system/media/LMprec_508.emd \
+vendor/disco/prebuilt/system/media/PFFprec_600.emd:system/media/PFFprec_600.emd
