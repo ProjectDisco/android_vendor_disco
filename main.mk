3b6180a511c455d2246cf2036d5d6c043ab2f135
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-12-07 15:42:30 +0200

    add prebuilt cLock widget
    needed for a feature by roman birg

--- main.mk
+++ main.mk
@@ -10,7 +10,8 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/disco/overlay
 PRODUCT_COPY_FILES += \
     vendor/disco/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
     vendor/disco/prebuilt/system/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/disco/prebuilt/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+    vendor/disco/prebuilt/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/disco/prebuilt/system/app/LockClock.apk:system/app/LockClock.apk
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.disco.version=$(ROM_VERSION) \
