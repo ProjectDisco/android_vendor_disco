a8d6c8580ffd3474dcff2e7415850efe2e193c99
Yashar Ahmedizade <yashade2001@gmail.com>
2015-07-18 13:34:11 +0200

    system_files.mk: Fix makefile error

--- configs/system_files.mk
+++ configs/system_files.mk
@@ -1,6 +1,5 @@
 PRODUCT_PACKAGE_OVERLAYS += vendor/disco/overlay/
 
 PRODUCT_COPY_FILES += \
-
 vendor/disco/prebuilt/system/etc/apns-conf.xml:system/etc/apns-conf.xml \
 vendor/disco/prebuilt/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so
