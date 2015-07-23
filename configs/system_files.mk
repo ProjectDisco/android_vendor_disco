ea494b39c6639408c7ad21334c26641c8b7057d7
Yashar Ahmedizadeh <yashade2001@gmail.com>
2015-07-23 14:57:40 +0300

    system_files.mk: Cleanup

--- configs/system_files.mk
+++ configs/system_files.mk
@@ -1,7 +1,15 @@
+# Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/disco/overlay/
 
+# APNs
 PRODUCT_COPY_FILES += \
-vendor/disco/prebuilt/system/etc/apns-conf.xml:system/etc/apns-conf.xml \
-vendor/disco/prebuilt/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+vendor/disco/prebuilt/system/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# LatinIME library
+PRODUCT_COPY_FILES += \
+vendor/disco/prebuilt/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
+# Camera effects
+PRODUCT_COPY_FILES += \
 vendor/disco/prebuilt/system/media/LMprec_508.emd:system/media/LMprec_508.emd \
 vendor/disco/prebuilt/system/media/PFFprec_600.emd:system/media/PFFprec_600.emd
