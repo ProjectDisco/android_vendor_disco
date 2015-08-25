271a7412e410030660996c248655489c034ea7e0
Yashar Ahmedizadeh <yashade2001@gmail.com>
2015-08-25 13:20:46 +0300

    products: Update fingerprints to LYZ28J

--- products/disco_mako.mk
+++ products/disco_mako.mk
@@ -16,7 +16,7 @@ PRODUCT_MANUFACTURER := LGE
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48I/1914015:user/release-keys \
+    BUILD_FINGERPRINT=google/occam/mako:5.1.1/LYZ28J/2074855:user/release-keys \
     PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48I 1914015 release-keys"
 
 endif
