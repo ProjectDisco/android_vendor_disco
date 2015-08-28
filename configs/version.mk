9fdb2e6a704f4512012e5f113214ac0de78cc356
Yashar Ahmedizadeh <yashade2001@gmail.com>
2015-08-28 17:45:16 +0300

    version.mk: Switch to semantic versioning
    Signed-off-by: Yashar Ahmedizadeh <yashade2001@gmail.com>

--- configs/version.mk
+++ configs/version.mk
@@ -1,5 +1,10 @@
+# Semantic versioning
+DISCO_VERSION_MAJOR := 2
+DISCO_VERSION_MINOR := 0
+DISCO_VERSION_MICRO := 0
+
 # ProjectDisco version number
-DISCO_VERSION := 2.0
+DISCO_VERSION := $(DISCO_VERSION_MAJOR).$(DISCO_VERSION_MINOR).$(DISCO_VERSION_MICRO)
 
 # Detailed version string
 ROM_VERSION := $(TARGET_PRODUCT)-$(DISCO_VERSION)-$(shell date -u +%Y%m%d)
