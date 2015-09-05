d63c38897054ffb9ec0d73515f928f00b289769f
Yashar Ahmedizade <yashade2001@gmail.com>
2015-09-05 13:30:45 +0300

    disco_emulator: Switch to x86 emulator
    because it runs faster.
    
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- products/disco_emulator.mk
+++ products/disco_emulator.mk
@@ -4,12 +4,12 @@ ifeq (disco_emulator,$(TARGET_PRODUCT))
 include vendor/disco/main.mk
 
 # Inherit device configuration
-$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, build/target/product/full_x86.mk)
 
 # Setup device specific product configuration
 PRODUCT_NAME := disco_emulator
 PRODUCT_BRAND := Android
-PRODUCT_DEVICE := generic
-PRODUCT_MODEL := ARM emulator
+PRODUCT_DEVICE := generic_x86
+PRODUCT_MODEL := x86 emulator
 
 endif
