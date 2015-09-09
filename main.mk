dc1b804175b586ad2ba5c8b200a60b63f3f2ab60
Yashar Ahmedizade <yashade2001@gmail.com>
2015-09-09 18:14:05 +0300

    Move global sepolicy inclusion to build/core (2/2)
    We need to be sure the global selinux policies are included last, to avoid accidental resetting by device configs.
    
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- main.mk
+++ main.mk
@@ -3,4 +3,3 @@ include vendor/disco/configs/packages.mk
 include vendor/disco/configs/prebuilts.mk
 include vendor/disco/configs/property_overrides.mk
 include vendor/disco/configs/version.mk
-include vendor/disco/configs/sepolicy.mk
