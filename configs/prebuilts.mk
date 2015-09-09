11e7ef10ad6017d5c62ff2c00a93e68ca2f669a1
Yashar Ahmedizade <yashade2001@gmail.com>
2015-09-09 15:11:29 +0300

    Revert "vendor: add camera effects prebuilts"
    This reverts commit 1fb72ee2a993e06d00e1f50116c7519dad1235d9
    
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- configs/prebuilts.mk
+++ configs/prebuilts.mk
@@ -2,11 +2,6 @@
 PRODUCT_COPY_FILES += \
 	vendor/disco/prebuilts/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# Camera effects
-PRODUCT_COPY_FILES += \
-	vendor/disco/prebuilts/media/LMprec_508.emd:system/media/LMprec_508.emd \
-	vendor/disco/prebuilts/media/PFFprec_600.emd:system/media/PFFprec_600.emd
-
 # LatinIME library for gesture typing
 PRODUCT_COPY_FILES += \
 	vendor/disco/prebuilts/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
