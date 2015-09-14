3f40afdadf41328dfbe63a6b4c9d49d11290d64c
Yashar Ahmedizade <yashade2001@gmail.com>
2015-09-14 14:27:16 +0300

    configs: Rename property_overrides.mk to properties.mk
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- main.mk
+++ main.mk
@@ -1,6 +1,6 @@
 include vendor/disco/configs/overlays.mk
 include vendor/disco/configs/packages.mk
 include vendor/disco/configs/prebuilts.mk
-include vendor/disco/configs/property_overrides.mk
+include vendor/disco/configs/properties.mk
 include vendor/disco/configs/version.mk
 include vendor/disco/configs/permissions.mk
