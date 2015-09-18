38b111602cd171478bc41f840d9ceb8849e5510b
Yashar Ahmedizade <yashade2001@gmail.com>
2015-09-18 13:23:59 +0300

    products: Remove emulator product
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -9,7 +9,3 @@ endif
 ifeq (disco_shamu,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/disco_shamu.mk
 endif
-
-ifeq (disco_emulator,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/disco_emulator.mk
-endif
