39e217605b831db4b985a72a2439c9e46e785b81
Yashar Ahmedizade <yashade2001@gmail.com>
2015-10-08 21:47:11 +0300

    Goodbye shamu (for now)
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,7 +1,3 @@
 ifeq (disco_hammerhead,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/disco_hammerhead.mk
 endif
-
-ifeq (disco_shamu,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/disco_shamu.mk
-endif
