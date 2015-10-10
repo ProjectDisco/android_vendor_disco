6d80b17f34a6c2ef958082bd8f54331dceeff06d
Yashar Ahmedizade <yashade2001@gmail.com>
2015-10-10 19:02:11 +0300

    bootanimation.mk: Simplify
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -1,11 +1,2 @@
-# Clear PRODUCT_BOOTANIMATION
-PRODUCT_BOOTANIMATION :=
-
 # Add bootanimation based on device resolution
-ifeq (1920x1080,$(TARGET_SCREEN_RES))
-PRODUCT_BOOTANIMATION := vendor/disco/prebuilts/bootanimation/bootanimation-1920x1080.zip
-endif
-
-ifeq (1280x720,$(TARGET_SCREEN_RES))
-PRODUCT_BOOTANIMATION := vendor/disco/prebuilts/bootanimation/bootanimation-1280x720.zip
-endif
+PRODUCT_BOOTANIMATION := vendor/disco/prebuilts/bootanimation/bootanimation-$(TARGET_SCREEN_RES).zip
