01eab9a73e4f2e19aad1eb0c454075009fe33614
Michael S Corigliano <michael.s.corigliano@gmail.com>
2015-10-13 02:14:52 +0000

    Revert "Goodbye shamu (for now)"
    This reverts commit 39e217605b831db4b985a72a2439c9e46e785b81.

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,3 +1,7 @@
 ifeq (disco_hammerhead,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/disco_hammerhead.mk
 endif
+
+ifeq (disco_shamu,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/disco_shamu.mk
+endif
---
+++ products/disco_shamu.mk
@@ -0,0 +1,25 @@
+ifeq (disco_shamu,$(TARGET_PRODUCT))
+
+# Screen resolution
+TARGET_SCREEN_RES := 1920x1080
+
+# Include ProjectDisco main configuration
+include vendor/disco/main.mk
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/full_shamu.mk)
+
+# Setup device specific product configuration
+PRODUCT_NAME := disco_shamu
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := shamu
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58K/2256973:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58K 2256973 release-keys"
+
+endif
