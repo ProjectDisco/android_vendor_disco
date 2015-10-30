1654a39fc1b25c5a2865de0442edb0ecaafd33e4
Yashar Ahmedizade <yashade2001@gmail.com>
2015-10-30 15:50:15 +0200

    Goodbye backup tool (1/2)
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>
    
    rm 50-disco.sh addon.d script
    
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- configs/prebuilts.mk
+++ configs/prebuilts.mk
@@ -5,10 +5,3 @@ PRODUCT_COPY_FILES += \
 # LatinIME library for gesture typing
 PRODUCT_COPY_FILES += \
 	vendor/disco/prebuilts/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
-
-# Backup tool support
-PRODUCT_COPY_FILES += \
-	vendor/disco/prebuilts/addon.d/50-disco.sh:system/addon.d/50-disco.sh \
-	vendor/disco/prebuilts/bin/backuptool.functions:system/bin/backuptool.functions \
-	vendor/disco/prebuilts/bin/backuptool.sh:system/bin/backuptool.sh
-
