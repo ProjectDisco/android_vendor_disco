3c1ba369ff4c92a253d5917d2e23a4002c7a8c88
Yashar Ahmedizade <yashade2001@gmail.com>
2016-09-23 19:44:43 +0000

    prebuilts: Remove LatinIME lib
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- configs/prebuilts.mk
+++ configs/prebuilts.mk
@@ -1,7 +1,3 @@
 # APNs
 PRODUCT_COPY_FILES += \
 	vendor/disco/prebuilts/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# LatinIME library for gesture typing
-PRODUCT_COPY_FILES += \
-	vendor/disco/prebuilts/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
