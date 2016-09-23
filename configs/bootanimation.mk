22d91ae33b41d717699e10f77ead0038b894266a
Yashar Ahmedizade <yashade2001@gmail.com>
2016-09-23 19:40:10 +0000

    Remove bootanimations
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- configs/bootanimation.mk
+++
@@ -1,2 +0,0 @@
-# Add bootanimation based on device resolution
-PRODUCT_BOOTANIMATION := vendor/disco/prebuilts/bootanimation/bootanimation-$(TARGET_SCREEN_RES).zip
--- main.mk
+++ main.mk
@@ -3,4 +3,3 @@ include vendor/disco/configs/packages.mk
 include vendor/disco/configs/prebuilts.mk
 include vendor/disco/configs/properties.mk
 include vendor/disco/configs/version.mk
-include vendor/disco/configs/bootanimation.mk
